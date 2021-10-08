a5a20cb59e1879aad10d809f6240e0bbef7f159f
Alex Percsi <139209+alexpercsi@users.noreply.github.com>
2021-10-08 17:05:08 +0200

    add environment variables

--- test.sh
+++ test.sh
@@ -3,11 +3,15 @@ REPORTS_DIR=$(pwd)/$ROBOT_REPORTS_DIR
 TESTS_DIR=$(pwd)/$ROBOT_TESTS_DIR
 sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
 
+environment_variable_array['VE_USERNAME','VE_PASSWORD']
+
 docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e BROWSER=$BROWSER \
   -e ROBOT_THREADS=$ROBOT_THREADS \
   -e PABOT_OPTIONS="$PABOT_OPTIONS" \
   -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
+  -e VE_USERNAME="$VE_USERNAME" \
+  -e VE_PASSWORD="$VE_PASSWORD" \
   -v $REPORTS_DIR:/opt/robotframework/reports:Z \
   -v $TESTS_DIR:/opt/robotframework/tests:Z \
   --user $(id -u):$(id -g) \
