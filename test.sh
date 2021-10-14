ed0ad5faab754f75e38f4c558f8ce1b888961210
Alex Percsi <139209+alexpercsi@users.noreply.github.com>
2021-10-14 16:58:12 +0200

    pass VE creds and URL over to RF through env vars

--- test.sh
+++ test.sh
@@ -3,15 +3,14 @@ REPORTS_DIR=$(pwd)/$ROBOT_REPORTS_DIR
 TESTS_DIR=$(pwd)/$ROBOT_TESTS_DIR
 sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
 
-environment_variable_array['VE_USERNAME','VE_PASSWORD']
-
 docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e BROWSER=$BROWSER \
   -e ROBOT_THREADS=$ROBOT_THREADS \
   -e PABOT_OPTIONS="$PABOT_OPTIONS" \
   -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
-  -e VE_USERNAME="$VE_USERNAME" \
-  -e VE_PASSWORD="$VE_PASSWORD" \
+  -e SELLICS_VENDOR_EMAIL="$SELLICS_VENDOR_EMAIL" \
+  -e SELLICS_VENDOR_PASSWORD="$SELLICS_VENDOR_PASSWORD" \
+  -e VENDOR_URL="$VENDOR_URL" \
   -v $REPORTS_DIR:/opt/robotframework/reports:Z \
   -v $TESTS_DIR:/opt/robotframework/tests:Z \
   --user $(id -u):$(id -g) \
